46369074b63aafeaa53c3dbb0308246373d0b566
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-05-13 18:40:06 +0430

    redux with hooks

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import CakeContainer from './components/CakeContainer';
 import {Provider} from "react-redux";
 import store from './components/redux/store';
+import HookCakeContainer from './components/HookCakeContainer';
 
 function App() {
   return (
     <div className="App">
 		<Provider store={store}>
 		<CakeContainer  />
+		<HookCakeContainer />
 		</Provider>
 
     </div>
---
+++ src/components/HookCakeContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useSelector , useDispatch} from "react-redux"
+import { buyCake } from './redux';
+
+
+
+
+const HookCakeContainer = () => {
+	const numOfCakes = useSelector (state => state.numOfCakes)
+	const dispatch = useDispatch()
+	return ( 
+		<div>
+			<h1>num of cakes  - {numOfCakes}</h1>
+			<button onClick = {()=>{dispatch(buyCake())}} >buy</button>
+		</div>
+	 );
+}
+ 
+export default HookCakeContainer;
